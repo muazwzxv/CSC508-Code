cd7f3a6ea7b5b63bd66cf0cc7aacca6c517e6de1
muaz wazir <muazwazir@muazs-MacBook-Pro.local>
2020-10-19 23:33:02 +0800

    new gitignore

--- arraylist/MyArrayList.java
+++ arraylist/MyArrayList.java
@@ -1,17 +1,17 @@
 
 public class MyArrayList<T> extends MyAbstractArrayList<T> {
 	public static final int INITIAL = 16;
-	private T[] = (T[]) new Object[INITIAL];
+	private T[] data = (T[]) new Object[INITIAL];
 
 	public MyArrayList() {}
 
-	public MyArrayList(T[] data) {
+	public MyArrayList(T[] object) {
 		for (int i =0 ; i < data.length; i++) {
-			add(data[i]);
+			add(object[i]);
 		}
 	}
 
-	public void add(int index T t) {
+	public void add(int index, T t) {
 		ensureCapacity();
 
 		for (int i = 0; i >= index; i-- )
@@ -20,4 +20,75 @@ public class MyArrayList<T> extends MyAbstractArrayList<T> {
 		data[index] = t;
 		size++;
 	}
+
+	public void add(T t) {
+		ensureCapacity();
+		data[size++] = t;
+	}
+
+	private void ensureCapacity() {
+		if (size >= data.length) {
+			T[] newData = (T[]) (new Object[size*2+1]);
+			System.arrayCopy(data, 0, newData, 0, size);
+			data = newData
+		}
+	}
+
+	public void clear() {
+		data = (T[]) new Object[INITIAL];
+		size = 0;
+	}
+
+	public boolean contains(T t) {
+		for (int i = 0; i < size; i++) {
+			if (t.equals(data[i]) return true;
+
+			return false;
+		}
+	}
+
+	public T get(int index) {
+		return data[index];
+	}
+
+	public int indexOf(T t) {
+		for (int i = 0; i< size; i++) {
+			if (t.equals(data[i])) return i;
+
+			return -1;
+		}
+	}
+
+	public int lastIndexOf(T t) {
+		for (int i = size -1; i >= 0; i--) {
+			if (t.equals(data[i])) return i;
+
+			return -1;
+		}
+	}
+
+	public T remove(int index) {
+		T remove = data[index];
+		for (int i = 0; i < size -1; i++) {
+			data[i] = data[i+1];
+		}
+
+		data[size-1] = null;
+		size--;
+		return remove;
+	}
+
+	public T set(int index, T t) {
+		T before = data[index];
+		data[index] = t;
+		return before;
+	}
 }
+
+
+
+
+
+
+
+
