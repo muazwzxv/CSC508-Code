29893a563e93e3689e8c62e82320d472bf40d015
muaz wazir <muazwazir@muazs-MacBook-Pro.local>
2020-10-20 01:08:58 +0800

    fix

--- arraylist/MyArrayList.java
+++ arraylist/MyArrayList.java
@@ -3,10 +3,11 @@ public class MyArrayList<T> extends MyAbstractArrayList<T> {
 	public static final int INITIAL = 16;
 	private T[] data = (T[]) new Object[INITIAL];
 
-	public MyArrayList() {}
+	public MyArrayList() {
+	}
 
 	public MyArrayList(T[] object) {
-		for (int i =0 ; i < data.length; i++) {
+		for (int i = 0; i < data.length; i++) {
 			add(object[i]);
 		}
 	}
@@ -14,11 +15,13 @@ public class MyArrayList<T> extends MyAbstractArrayList<T> {
 	public void add(int index, T t) {
 		ensureCapacity();
 
-		for (int i = 0; i >= index; i-- )
-			data[i+1] = data[i]
+		for (int i = 0; i >= index; i--)
+			data[i + 1] = data[i];
 
 		data[index] = t;
 		size++;
+
+		return;
 	}
 
 	public void add(T t) {
@@ -28,9 +31,9 @@ public class MyArrayList<T> extends MyAbstractArrayList<T> {
 
 	private void ensureCapacity() {
 		if (size >= data.length) {
-			T[] newData = (T[]) (new Object[size*2+1]);
-			System.arrayCopy(data, 0, newData, 0, size);
-			data = newData
+			T[] newData = (T[]) (new Object[size * 2 + 1]);
+			System.arraycopy(data, 0, newData, 0, size);
+			data = newData;
 		}
 	}
 
@@ -41,10 +44,11 @@ public class MyArrayList<T> extends MyAbstractArrayList<T> {
 
 	public boolean contains(T t) {
 		for (int i = 0; i < size; i++) {
-			if (t.equals(data[i]) return true;
+			if (t.equals(data[i]))
+				return true;
 
-			return false;
 		}
+		return false;
 	}
 
 	public T get(int index) {
@@ -52,28 +56,28 @@ public class MyArrayList<T> extends MyAbstractArrayList<T> {
 	}
 
 	public int indexOf(T t) {
-		for (int i = 0; i< size; i++) {
-			if (t.equals(data[i])) return i;
-
-			return -1;
+		for (int i = 0; i < size; i++) {
+			if (t.equals(data[i]))
+				return i;
 		}
+		return -1;
 	}
 
 	public int lastIndexOf(T t) {
-		for (int i = size -1; i >= 0; i--) {
-			if (t.equals(data[i])) return i;
-
-			return -1;
+		for (int i = size - 1; i >= 0; i--) {
+			if (t.equals(data[i]))
+				return i;
 		}
+		return -1;
 	}
 
 	public T remove(int index) {
 		T remove = data[index];
-		for (int i = 0; i < size -1; i++) {
-			data[i] = data[i+1];
+		for (int i = 0; i < size - 1; i++) {
+			data[i] = data[i + 1];
 		}
 
-		data[size-1] = null;
+		data[size - 1] = null;
 		size--;
 		return remove;
 	}
@@ -87,26 +91,20 @@ public class MyArrayList<T> extends MyAbstractArrayList<T> {
 	public String toString() {
 		StringBuilder result = new StringBuilder("[");
 
-		for (int i =0 ; i < size(); i++) {
+		for (int i = 0; i < size(); i++) {
 			result.append(data[i]);
-			if (i < size -1) result.append(". ");
+			if (i < size - 1)
+				result.append(". ");
 		}
 		return result.toString() + "]";
 	}
 
 	public void trimToSize() {
 		if (size != data.length) {
-			T[] newaData = (T[]) (new Object[size]);
-			System.arraycopy(data, 0);
+			T[] newData = (T[]) (new Object[size]);
+			System.arraycopy(data, 0, newData, 0, size);
 			data = newData;
 		}
+		return;
 	}
 }
-
-
-
-
-
-
-
-
