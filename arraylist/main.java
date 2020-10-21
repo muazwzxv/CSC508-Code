3ab2cb71ffb6c787b631dc15556c86bae3292453
muaz wazir <muazwazir@muazs-MacBook-Pro.local>
2020-10-21 13:16:13 +0800

    generic

--- arraylist/main.java
+++ arraylist/main.java
@@ -3,9 +3,22 @@ public class main {
 
 		Student data = new Student("muaz", 21, "CS230", 4);
 
+		// with generic type safety
 		MyArrayList<Student> list = new MyArrayList<Student>();
 		list.add(data);
 
+		// With non generic type safety
+		MyArrayList list2 = new MyArrayList();
+		list2.add(3);
+		list2.add(data);
+
+		System.out.println(list2);
 		System.out.println(list.get(0).getName());
+	}
+
+	static Student[] seed() {
+		Student[] data = new Student[10];
+
+		return data;
 	}
 }
