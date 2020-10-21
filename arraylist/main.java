433ec8f4e565cb7401b29220a070c8a6793cd720
muaz wazir <muazwazir@muazs-MacBook-Pro.local>
2020-10-21 15:25:02 +0800

    seed

--- arraylist/main.java
+++ arraylist/main.java
@@ -6,19 +6,34 @@ public class main {
 		// with generic type safety
 		MyArrayList<Student> list = new MyArrayList<Student>();
 		list.add(data);
+		System.out.println("Generic \n" + list);
 
 		// With non generic type safety
 		MyArrayList list2 = new MyArrayList();
 		list2.add(3);
 		list2.add(data);
+		list2.add("kontolan rasukan barney and friends");
+		System.out.println("Non Generic \n" + list2);
 
-		System.out.println(list2);
-		System.out.println(list.get(0).getName());
+		MyArrayList<Student> seeded = seed();
+		System.out.println("From seeded \n" + seeded);
 	}
 
-	static Student[] seed() {
-		Student[] data = new Student[10];
+	static MyArrayList<Student> seed() {
+		MyArrayList<Student> list = new MyArrayList<Student>();
 
-		return data;
+		list.add(new Student("Muaz", 21, "CSC230", 4));
+		list.add(new Student("Syuk", 21, "CSC230", 4));
+		list.add(new Student("Hakim", 21, "CSC230", 4));
+		list.add(new Student("Amsyar", 21, "CSC230", 4));
+		list.add(new Student("Dayang", 21, "CSC230", 4));
+		list.add(new Student("Mizan", 21, "CSC230", 4));
+		list.add(new Student("Alep", 21, "CSC230", 4));
+		list.add(new Student("Ajim", 21, "CSC230", 4));
+		list.add(new Student("Jebat", 21, "CSC230", 4));
+		list.add(new Student("Ajiq", 21, "CSC230", 4));
+		list.add(new Student("Zetty", 21, "CSC230", 4));
+
+		return list;
 	}
 }
