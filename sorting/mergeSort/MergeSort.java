c76e032bd99a4ca8bd915b4da0d67f763f0c7ea0
Muaz Wazir <muazwazir@gmail.com>
2021-01-10 03:27:53 +0800

    lint

--- sorting/mergeSort/MergeSort.java
+++ sorting/mergeSort/MergeSort.java
@@ -6,24 +6,32 @@ public class MergeSort {
 
     int [] arr = {12, 21, 40, -5, 40 ,34, 75};
 
+    // sort the array
     sort(arr);
-
+    
+    // Display
     for (int i: arr) System.out.println(i+"");
   }
 
   public static void sort(int arr[]) {
+
+    // Base case if the size divided subArray is less than two
     if (arr.length < 2) return;
 
     int mid = arr.length / 2;
     int [] l = new int[mid];
     int [] r = new int[arr.length - mid];
 
+    // split input array to leftSide = [start .... mid]
     for (int i = 0; i < mid; i++) l[i] = arr[i];
+    // split input array to rightSide = [mid ..... end]
     for (int i = mid; i < arr.length; i++) r[i - mid] = arr[i];
 
+    // recursively call the function for both subArray
     sort(l);
     sort(r);
-
+    
+    // Merge all subArray into one whole sorted array
     merge(arr, l, r, mid, arr.length - mid);
   }
 
@@ -34,11 +42,16 @@ public class MergeSort {
     int k = 0;
 
     while (i < left && j < right) {
-      if (leftArray[i] <= rightArray[j]) arr[k++] = leftArray[i++];
-      else arr[k++] = rightArray[j++];
+      if (leftArray[i] <= rightArray[j]) 
+        arr[k++] = leftArray[i++];
+      else 
+        arr[k++] = rightArray[j++];
     }
 
-    while(i < left) arr[k++] = leftArray[i++];
-    while(j < right) arr[k++] = rightArray[j++];
+    while(i < left) 
+      arr[k++] = leftArray[i++];
+
+    while(j < right) 
+      arr[k++] = rightArray[j++];
   }
 }
